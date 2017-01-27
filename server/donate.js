8cba1ecfbbca9b9c4ee37039944d4f6de3852a9a
meegan <matt.l.egan@gmail.com>
2017-01-27 17:13:36 -0500

    breaking change in the serverside donate function

--- server/donate.js
+++ server/donate.js
@@ -1,4 +1,4 @@
-var stripe = require('stripe')('sk_test_D32Dl92AC6IWj1MydXgEuG75';
+var stripe = require('stripe')('sk_test_D32Dl92AC6IWj1MydXgEuG75');
 
 module.exports = function(req, res) {
 	res.send('POSt request to the home page');
