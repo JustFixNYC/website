63688249d9e89008bf90e897f2a3cb4fca4f8fdc
Jenn Scheer <singleportrait@gmail.com>
2018-04-19 12:28:56 -0400

    Add documentation for the filter

--- src/app/filters/trustUrl.js
+++ src/app/filters/trustUrl.js
@@ -1,3 +1,14 @@
+/**
+ * @ngdoc filter
+ * @name trustUrl
+ * @kind function
+ *
+ * @description
+ * Trust a resource URL without having to run $sce in each individual controller
+ *
+ * @param {string} url URL to trust
+ * @returns {string} Trusted URL
+ */
 (function() {
   angular.module('boilerplate').filter('trustUrl', function ($sce) {
       return function(url) {
